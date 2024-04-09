ffff4c12da82652b5f9ffbc279add29fcd665cb3
dmandache <diana.mandache00@gmail.com>
2024-04-09 12:10:52 +0200

    Changes to be committed: 	modified:   main.py 	modified:   test_pythae.ipynb

--- main.py
+++ main.py
@@ -8,10 +8,30 @@ from pythae.pipelines.training import TrainingPipeline
 from pythae.trainers.training_callbacks import WandbCallback
 from pythae.samplers import NormalSampler, GaussianMixtureSampler, GaussianMixtureSamplerConfig
 from pythae.samplers import RHVAESampler, RHVAESamplerConfig
+from pythae.models.nn.benchmarks.mnist import *
+from pythae.models.nn.default_architectures import *
 import wandb
 import argparse
 import random
 from datetime import datetime
+
+architecture_dict = {
+    'mlp':
+        {
+        'encoder': Encoder_VAE_MLP,
+        'decoder': Decoder_AE_MLP,
+        },
+    'convnet':
+        {
+        'encoder': Encoder_Conv_VAE_MNIST,
+        'decoder': Decoder_Conv_AE_MNIST,
+        },
+    'resnet':
+        {
+        'encoder': Encoder_ResNet_VAE_MNIST,
+        'decoder': Decoder_ResNet_AE_MNIST,
+        },
+    }
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
@@ -46,31 +66,22 @@ def sample_indices(vector, k, seed=None):
 
     return indices
 
-
-class MNIST(Dataset):
-    def __init__(self, data):
-        self.data = data.type(torch.float)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        x = self.data[index]
-        return DatasetOutput(data=x)
     
 if __name__ == '__main__':
 
     # Argument Parser
     parser = argparse.ArgumentParser() #description='Train a RHVAE with synthetic data generation.')
     parser.add_argument('--input_dim', type=int, default=28, help='Dimensionality of the input data')
-    parser.add_argument('--latent_dim', type=int, default=8, help='Dimensionality of the latent space')
+    parser.add_argument('--latent_dim', type=int, default=16, help='Dimensionality of the latent space')
     parser.add_argument('--n_runs', type=int, default=3, help='Number of degenerating runs')
     parser.add_argument('--n_train', type=int, default=20, help='Number of training samples per class')
     parser.add_argument('--n_test', type=int, default=100, help='Number of test samples per class')
     parser.add_argument('--k', type=int, default=200, help='Number of synthetic data samples to generate at each iteration')
     parser.add_argument('--sampler', choices=['normal', 'gmm', 'rhvae'], default='rhvae', help='Sampler type for generating synthetic data')
-    parser.add_argument('--model', choices=['CNN', 'MLP'], default='MLP', help='Model Architecture')
-    parser.add_argument('--n_epochs', type=int, default=10, help='Number of training epochs for each run')
+    parser.add_argument('--model', choices=['convnet','resnet', 'mlp'], default='resnet', help='Model Architecture')
+    parser.add_argument('--n_epochs', type=int, default=100, help='Number of training epochs for each run')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('--batch_size', type=int, default=200, help='Batch size')
     args = parser.parse_args()
 
     # Set device
@@ -79,16 +90,22 @@ if __name__ == '__main__':
     # Model Config
     model_config = RHVAEConfig(
         input_dim=(1, args.input_dim, args.input_dim),
-        latent_dim=args.latent_dim
+        latent_dim=args.latent_dim,
+        n_lf=1,
+        eps_lf=0.001,
+        beta_zero=0.3,
+        temperature=1.5,
+        regularization=0.001
+
     )
 
     # Training Config
     training_config = BaseTrainerConfig(
-        output_dir='experiments',
+        output_dir=f'experiments/{timestamp}',
         num_epochs=args.n_epochs,
-        learning_rate=3e-3,
-        per_device_train_batch_size=100,
-        per_device_eval_batch_size=64,
+        learning_rate=args.lr,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
     )
 
     # Real data loader
@@ -126,7 +143,9 @@ if __name__ == '__main__':
                    "Evaluation Data": wandb.Image(eval_dataset)})
 
         model = RHVAE(
-            model_config=model_config
+            model_config=model_config,
+            encoder=architecture_dict[args.model]['encoder'](model_config), 
+            decoder=architecture_dict[args.model]['decoder'](model_config) 
         )
 
         # trainer = BaseTrainer(
@@ -171,6 +190,9 @@ if __name__ == '__main__':
             )
         elif args.sampler == 'rhvae':
             sampler_config = RHVAESamplerConfig(
+                mcmc_steps_nbr=100,
+                n_lf=10,
+                eps_lf=0.03
             )
 
             sampler = RHVAESampler(
@@ -189,3 +211,5 @@ if __name__ == '__main__':
         # Update Training Dataset with Generated Data
         #train_dataset = ConcatDataset([train_dataset, gen_data])
         train_dataset = torch.cat((train_dataset, gen_data), 0)
+        # shuffle
+        train_dataset = train_dataset[torch.randperm(train_dataset.size()[0])]
